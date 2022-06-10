e6916f7fbf9ce7c19c648ebfceb6e00b43c733c8
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-09 20:28:23 -0600

    Expose SSL_VERIFY as env var

--- auth.py
+++ auth.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 
 import requests
+import os
 
 class Auth:
     auth = {}
+    ssl_verify = True
 
     def __init__(self):
-        pass
+        if os.getenv('SSL_VERIFY'):
+            self.ssl_verify = bool(int(os.getenv('SSL_VERIFY')))
 
     def login(self, nick, username, password):
         if '{}'.format(nick) in self.auth and self.auth[nick]['character'].lower() == username.lower():
             return True
-        response = requests.post("https://rpg.swiftirc.net/me", data={'name': username, 'password': password}, verify=False)
+
+        headers = {'Authorization': 'Bearer {}'.format(os.getenv('API_TOKEN'))}
+
+        response = requests.post("https://rpg.swiftirc.net/api/auth", data={'name': username, 'password': password}, verify=self.ssl_verify, headers=headers)
+
         if response.status_code == 200:
             self.auth[nick] = {'character': username}
             return True
