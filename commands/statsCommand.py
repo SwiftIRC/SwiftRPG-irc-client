f3651f63cde7f9a231a7702d1c6ca2cdebe68551
Kyle.Tryon <Kyle.Tryon@outlook.com>
2022-06-14 23:49:02 -0400

    fix: add game

--- commands/statsCommand.py
+++ commands/statsCommand.py
@@ -4,9 +4,9 @@ async def exec(game, command, target, author, message, character, token):
     args = message.split()[0][1:]
 
     if len(args) != 2:
-        response = await api.get(command, target, token, 'stats')
+        response = await api.get(game, command, target, token, 'stats')
     else:
         character = args[1]
-        response = await api.get(command, target, token, 'stats/{}'.format(args[1]))
+        response = await api.get(game, command, target, token, 'stats/{}'.format(args[1]))
 
     return "Stats: {} - Thieving: {} - Woodcutting: {}".format(character, response['thieving'], response['woodcutting'])
