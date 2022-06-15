afc9e1e08f1f1f1ea153e026845a1db6cd66493d
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-15 10:38:34 -0600

    Process a .look response

--- commands/map/LookCommand.py
+++ commands/map/LookCommand.py
@@ -6,4 +6,4 @@ import API.api as api
 async def exec(game, command, target, author, message, character, token):
     response = await api.get(game, command, target, token, 'map/user/look')
     if response:
-        return "[{}] 👀 Looking around: {}".format(character, response)
+        return "[{}] 👀 Looking around [{}, {}]. Trees: {} - NPCs: {}".format(character, response['x'], response['y'], response['available_trees'], len(response['npcs']))
