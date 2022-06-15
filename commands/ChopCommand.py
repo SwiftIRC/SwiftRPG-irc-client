bae860f8e79b56d05707a3eeff1e6f75051ec651
Kyle.Tryon <Kyle.Tryon@outlook.com>
2022-06-14 23:44:26 -0400

    feat: add stats command

--- commands/ChopCommand.py
+++ commands/ChopCommand.py
@@ -1,6 +1,5 @@
 import API.api as api
 
-
 async def exec(game, command, target, author, message, character, token):
     response = await api.post(game, command, target, token, 'woodcutting/chop')
     if response:
