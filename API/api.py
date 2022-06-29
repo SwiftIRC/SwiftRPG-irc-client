2996860201ab031843bd344948c3ec5395988c26
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-29 16:14:18 -0600

    fix: Error handling in the POST function

--- API/api.py
+++ API/api.py
@@ -6,17 +6,22 @@ import string
 
 
 # The `target` parameter can be either a string (from IRC) or a function (from Discord)
-
-
 async def post(self, command: FunctionType, target, token: string, endpoint: string, data={}):
     headers = {'Authorization': 'Bearer {}'.format(token),
                'X-Bot-Token': os.getenv('API_TOKEN')}
 
-    response = requests.post("{}/api/{}".format(os.getenv('HOSTNAME'), endpoint),
-                             data=data,
-                             verify=self.ssl_verify,
-                             headers=headers,
-                             allow_redirects=False)
+    try:
+        response = requests.post("{}/api/{}".format(os.getenv('HOSTNAME'), endpoint),
+                                 data=data,
+                                 verify=self.ssl_verify,
+                                 headers=headers,
+                                 allow_redirects=False)
+    except requests.exceptions.SSLError:
+        await self.process_response(command, target, "Error: encountered an SSL error!")
+        return
+    except requests.exceptions.ConnectionError:
+        await self.process_response(command, target, "Error: connection error. Please try again in a few minutes.")
+        return
 
     if response.status_code == 302:
         await self.process_response(command, target, "Error: user session expired. Please PM the bot `.logout` and then log back in.")
@@ -26,13 +31,13 @@ async def post(self, command: FunctionType, target, token: string, endpoint: str
         try:
             return response.json()
         except json.decoder.JSONDecodeError:
-            print("ERROR: api.py [29]: ", response.text)
+            print("ERROR: api.py [01]: ", response.text)
             await self.process_response(command, target, "Error: {}".format(response.json().get('error', 'unknown [02]')))
         except Exception as e:
-            print("ERROR: api.py [32]: ", e)
+            print("ERROR: api.py [02]: ", e)
             await self.process_response(command, target, "Error: {}".format(response.json().get('error', 'unknown [03]')))
     else:
-        print("ERROR: api.py [35]: ",
+        print("ERROR: api.py [03]: ",
               response.status_code, response.text)
 
 
@@ -40,10 +45,17 @@ async def get(self, command: FunctionType, target, token: string, endpoint: stri
     headers = {'Authorization': 'Bearer {}'.format(token),
                'X-Bot-Token': os.getenv('API_TOKEN')}
 
-    response = requests.get("{}/api/{}".format(os.getenv('HOSTNAME'), endpoint),
-                            verify=self.ssl_verify,
-                            headers=headers,
-                            allow_redirects=False)
+    try:
+        response = requests.get("{}/api/{}".format(os.getenv('HOSTNAME'), endpoint),
+                                verify=self.ssl_verify,
+                                headers=headers,
+                                allow_redirects=False)
+    except requests.exceptions.SSLError:
+        await self.process_response(command, target, "Error: encountered an SSL error.")
+        return
+    except requests.exceptions.ConnectionError:
+        await self.process_response(command, target, "Error: connection error. Please try again in a few minutes.")
+        return
 
     if response.status_code == 302:
         await self.process_response(command, target, "Error: user session expired. Please PM the bot `.logout` and then log back in.")
@@ -53,11 +65,11 @@ async def get(self, command: FunctionType, target, token: string, endpoint: stri
         try:
             return response.json()
         except json.decoder.JSONDecodeError:
-            print("ERROR: api.py [56]: ", response.text)
+            print("ERROR: api.py [04]: ", response.text)
             await self.process_response(command, target, "Error: {}".format(response.json().get('error', 'unknown [04]')))
         except Exception as e:
-            print("ERROR: api.py [59]: ", e)
+            print("ERROR: api.py [05]: ", e)
             await self.process_response(command, target, "Error: {}".format(response.json().get('error', 'unknown [05]')))
     else:
-        print("ERROR: api.py [62]: ",
+        print("ERROR: api.py [06]: ",
               response.status_code, response.text)
