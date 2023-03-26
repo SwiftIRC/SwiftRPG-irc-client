bdbf6e070f774d80f89fa39f0aebbf8235b6c762
Ryan William O'Hara <ryan@ryanwohara.com>
2023-03-26 15:24:04 -0600

    chore: Slight cleanup

--- commands/firemaking/BurnCommand.py
+++ commands/firemaking/BurnCommand.py
@@ -12,10 +12,8 @@ async def exec(game, command, target, author, message, character, token):
                 character,
                 prefix,
                 await game.level(response.get('experience')),
-                response.get(
-                    'experience'),
-                response.get('reward', {}).get(
-                    'total', 0)
+                response.get('experience'),
+                response.get('reward', {}).get('total', 0)
             )
         elif 'error' in response:
             return "[{}] {}: {}".format(
--- commands/woodcutting/ChopCommand.py
+++ commands/woodcutting/ChopCommand.py
@@ -5,12 +5,10 @@ import API.api as api
 
 async def exec(game, command, target, author, message, character, token):
     response = await api.post(game, command, target, token, 'woodcutting/chop')
-    if response:
+    if 'experience' in response:
         return "[{}] 🪓 Woodcutting: {} ({}xp) - Logs: {}".format(
             character,
-            await game.level(response.get('experience', 0)),
-            response.get(
-                'experience', 0),
-            response.get('reward', {}).get(
-                'total', 0)
+            await game.level(response.get('experience')),
+            response.get('experience'),
+            response.get('reward', {}).get('total', 0)
         )
