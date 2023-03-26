1d3003cdf6a24aa0a371837601c74a43fee6da38
Ryan William O'Hara <ryan@ryanwohara.com>
2023-03-25 21:40:32 -0600

    refactor: Use new formats for chop and pickpocket

--- commands/thieving/PickpocketCommand.py
+++ commands/thieving/PickpocketCommand.py
@@ -7,7 +7,12 @@ async def exec(game, command, target, author, message, character, token):
     response = await api.post(game, command, target, token, 'thieving/pickpocket')
     if response:
         prefix = '🕵️ Pickpocketing'
-        if response.get('original', response).get('thieving', 0):
-            return "[{}] {}: {} ({}xp) - Gold: {}".format(character, prefix, await game.level(response.get('original', response).get('thieving', 0)), response.get('original', response).get('thieving', 0), response.get('original', response).get('gold', 0))
+        if response.get('original', response).get('experience', 0):
+            return "[{}] {}: {} ({}xp) - Gold: {}".format(character,
+                                                          prefix,
+                                                          await game.level(response.get('original', response).get('experience', 0)),
+                                                          response.get('original', response).get(
+                                                              'experience', 0),
+                                                          response.get('original', response).get('reward', 0).get('total', 0))
         elif response.get('error'):
             return "[{}] {}: {} ({} hitpoints remaining)".format(character, prefix, response.get('error', ''), response.get('hitpoints', -1))
