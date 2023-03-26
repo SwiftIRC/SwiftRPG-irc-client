3600630e47ad62bb0dfb1b3ec8d9b87d9590c409
Ryan William O'Hara <ryan@ryanwohara.com>
2023-03-25 21:42:31 -0600

    chore: Remove unused code

--- commands/woodcutting/ChopCommand.py
+++ commands/woodcutting/ChopCommand.py
@@ -6,4 +6,8 @@ import API.api as api
 async def exec(game, command, target, author, message, character, token):
     response = await api.post(game, command, target, token, 'woodcutting/chop')
     if response:
-        return "[{}] 🪓 Woodcutting: {} ({}xp) - Logs: {}".format(character, await game.level(response.get('original', response).get('experience', 0)), response.get('original', response).get('experience', 0), response.get('original', response).get('reward', 0).get('total', 0))
+        return "[{}] 🪓 Woodcutting: {} ({}xp) - Logs: {}".format(character,
+                                                                 await game.level(response.get('experience', 0)),
+                                                                 response.get(
+                                                                     'experience', 0),
+                                                                 response.get('reward', {}).get('total', 0))
