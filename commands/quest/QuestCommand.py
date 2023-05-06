9c9ce0b7d54114ef9494491f1d1ec38dbefa3874
Ryan William O'Hara <ryan@ryanwohara.com>
2023-05-05 21:33:16 -0600

    fix: Stats and quest commands

--- commands/quest/QuestCommand.py
+++ commands/quest/QuestCommand.py
@@ -19,14 +19,14 @@ async def exec(
     split = message.split()
 
     if len(split) == 1:
-        return "[{0}] 🗺️ Syntax: {1} list | {1} start [Quest ID] (Step ID) | {1} inspect [Quest ID]".format(
+        return "[{0}] 📜 Syntax: {1} list | {1} start [Quest ID] (Step ID) | {1} inspect [Quest ID]".format(
             character, split[0]
         )
     elif len(split) >= 2:
         if split[1] == "list":
             response = await api.get(game, command, target, token, "quests")
 
-            if response:
+            if response and len(response):
                 quests = []
                 for index in response:
                     quest = Box(index)
@@ -35,31 +35,39 @@ async def exec(
                             str(quest.id), quest.name, quest.completed, quest.total
                         )
                     )
-                return "[{}] 🗺️ Quests | {}".format(character, ", ".join(quests))
-            return "[{}] 🗺️ Quests | None started".format(character)
+                return "[{}] {} {} | {}".format(
+                    character,
+                    "📜",
+                    "Questing",
+                    ", ".join(quests),
+                )
+            return "[{}] 📜 Questing | None started".format(character)
         elif split[1] == "start":
             if len(split) < 3:
-                return "[{}] 🗺️ Syntax: {} start [Quest ID] (Step ID)".format(
+                return "[{}] 📜 Syntax: {} start [Quest ID] (Step ID)".format(
                     character, split[0]
                 )
             path = "quests/start/{}/{}".format(
                 split[2], "" if len(split) < 4 else split[3]
             )
             response = await api.get(game, command, target, token, path)
+            r = Box(response)
 
             if response:
                 if response["failure"] != None:
-                    return "[{}] 🗺️ Error: {}".format(character, response["failure"])
-                quest = response["metadata"]["response"]
-                if quest.get("incompleteDependencies", 0) > 0:
-                    incompleteSteps = [
-                        str(step["id"]) for step in quest.get("incompleteSteps", [])
+                    return "[{}] 📜 Error: {}".format(character, response["failure"])
+                quest = Box(response["metadata"])
+                if quest.incomplete_dependencies > 0:
+                    incomplete_steps = [
+                        str(step["id"]) for step in quest.incomplete_steps
                     ]
-                    plural = "s" if quest["incompleteDependencies"] > 1 else ""
-                    return (
-                        "[{}] 🗺️ Quests: Quest dependencies not met | {} step{}".format(
-                            character, str(quest["incompleteDependencies"]), plural
-                        )
+                    plural = "s" if quest.incomplete_dependencies > 1 else ""
+                    return "[{}] {} {}: Quest dependencies not met | {} step{}".format(
+                        r.user.name,
+                        r.command.emoji,
+                        r.command.name.title(),
+                        str(quest.incomplete_dependencies),
+                        plural,
                     )
                 rewards = {
                     reward: quest[reward]
@@ -68,41 +76,40 @@ async def exec(
                     or reward in common.lib.skills
                     and quest[reward] > 0
                 }
-                quests = "{}: {} | {} {}".format(
-                    str(quest.get("id")),
-                    quest.get("name"),
-                    quest.get("step", {}).get("output"),
-                    "(repeated)" if quest.get("completeStep", None) != None else "",
+                quests = "{}: {} | {}".format(
+                    str(quest.details.id),
+                    quest.details.name,
+                    quest.step_details.output,
                 )
-                return "[{}] 🗺️ Quests: {}".format(character, quests)
+                return "[{}] {} {}: {}".format(
+                    r.user.name, r.command.emoji, r.command.name, quests
+                )
         elif split[1] == "inspect":
             if len(split) < 3:
-                return "[{}] 🗺️ Syntax: {} inspect [Quest ID]".format(
+                return "[{}] 📜 Syntax: {} inspect [Quest ID]".format(
                     character, split[0]
                 )
             path = "quests/inspect/{}".format(split[2])
             response = await api.get(game, command, target, token, path)
 
             if response:
-                quest = response["metadata"]["response"]
+                quest = response["metadata"]
 
                 rewards = {
-                    reward: quest[reward]
-                    for reward in quest
-                    if reward == "gold"
-                    or reward in common.lib.skills
-                    and quest[reward] > 0
+                    reward["details"]["name"].title(): reward["gained"]
+                    for reward_type in response["reward"]
+                    for reward in response["reward"][reward_type]
                 }
                 quests = []
                 quests.append(
                     "{}: {} | {} | Completion: {}/{} | Rewards: {}".format(
-                        str(quest.get("id")),
-                        quest.get("name"),
-                        quest.get("description"),
-                        len(quest.get("completeSteps", [])),
-                        len(quest.get("incompleteSteps", []))
-                        + len(quest.get("completeSteps", [])),
-                        " ~ ".join(
+                        str(quest["details"]["id"]),
+                        quest["details"]["name"],
+                        quest["details"]["description"],
+                        len(quest.get("complete_steps", [])),
+                        len(quest.get("incomplete_steps", []))
+                        + len(quest.get("complete_steps", [])),
+                        ", ".join(
                             [
                                 "{}: {}".format(reward, rewards[reward])
                                 for reward in rewards
@@ -111,4 +118,4 @@ async def exec(
                         ),
                     )
                 )
-                return "[{}] 🗺️ Quests: {}".format(character, ", ".join(quests))
+                return "[{}] 📜 Quests: {}".format(character, ", ".join(quests))
--- commands/stats/StatsCommand.py
+++ commands/stats/StatsCommand.py
@@ -6,23 +6,23 @@ import API.api as api
 async def exec(game, command, target, author, message, character, token):
     args = message.split()
 
-    character = ''
+    character = ""
     if len(args) >= 2:
         character = args[1]
 
-    response = await api.get(game, command, target, token, 'stats/{}'.format(character))
+    response = await api.get(game, command, target, token, "stats/{}".format(character))
 
     if response:
         return "[{}] {}".format(
-            response['user']['name'],
-            ' | '.join(
+            response["user"]["name"],
+            " | ".join(
                 [
-                    '{}: {} ({:,}xp)'.format(
-                        skill['details']['name'].title(),
-                        await game.level(skill['total']),
-                        skill['total'],
+                    "{}: {} ({:,}xp)".format(
+                        skill["details"]["name"].title(),
+                        await game.level(skill["gained"]),
+                        skill["gained"],
                     )
-                    for skill in response['skills']
+                    for skill in response["skills"]
                 ]
             ),
         )
